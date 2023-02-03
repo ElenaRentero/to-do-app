02f1e9a544b1b393daf4fd33b1fcc98d3e49fc48
Elena Rentero <elenaclementerentero@gmail.com>
2023-02-03 13:16:02 +0100

    Fix mediaqueries mobile

--- src/components/tasks/TasksList.jsx
+++ src/components/tasks/TasksList.jsx
@@ -6,9 +6,16 @@ function TasksList ({ displayTasks, capitalizeFirstLetter, deleteTask, changeSta
   const listOfTasks = displayTasks.map((task, i) => {
     return <TaskCard task={task} key={i} capitalizeFirstLetter={capitalizeFirstLetter} deleteTask={deleteTask} changeStatusTask={changeStatusTask} />
   })
+
+  function isMobileDevice () {
+    return (typeof window.orientation !== 'undefined') || (navigator.userAgent.indexOf('IEMobile') !== -1)
+  };
+
+  const lengthThreshold = isMobileDevice() ? 3 : 5
+
   return (
-    <TableContainer width='82.5%' height='305px' overflowX='hidden' marginTop='10px' style={{ overflowY: displayTasks.length > 5 ? 'scroll' : 'hidden' }}>
-      <Table variant='simple' className='table'>{listOfTasks}</Table>
+    <TableContainer className='table' width='82.5%' overflowX='hidden' marginTop='10px' style={{ overflowY: displayTasks.length > lengthThreshold ? 'scroll' : 'hidden' }}>
+      <Table variant='simple' className='table__container'>{listOfTasks}</Table>
     </TableContainer>
   )
 }
